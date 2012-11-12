350d0cbeea10120bca1046e83b0b177d89dcd273
sursrc <sursrc@gmail.com>
2012-11-12 15:56:36 +0800

    first commit

---
+++ Problems/ListViewController.m
@@ -0,0 +1,73 @@
+
+#import "ListViewController.h"
+
+@implementation ListViewController
+{
+	NSMutableArray *list;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    if (self == [super initWithCoder:aDecoder]) {
+        list = [[NSMutableArray arrayWithCapacity:10] retain];
+        
+        [list addObject:@"One"];
+        [list addObject:@"Two"];
+        [list addObject:@"Three"];
+        [list addObject:@"Four"];
+        [list addObject:@"Five"];
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [list release];
+    [super dealloc];
+}
+
+- (void)viewDidLoad
+{
+	[super viewDidLoad];
+    NSLog(@"viewDidLoad is called");
+}
+
+- (void)viewDidUnload
+{
+	[super viewDidUnload];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+	return [list count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	static NSString *CellIdentifier = @"Cell";
+	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+	cell.textLabel.text = [list objectAtIndex:indexPath.row];
+
+    NSLog(@"array contents: %@", list);
+    
+	return cell;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	if (editingStyle == UITableViewCellEditingStyleDelete)
+	{
+        [list removeObjectAtIndex:indexPath.row];
+		[tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+	}   
+}
+
+@end
---
+++ Problems/MainViewController.m
@@ -0,0 +1,31 @@
+
+#import "MainViewController.h"
+
+@implementation MainViewController
+
+@synthesize list = _list;
+@synthesize button = _button;
+
+- (void)viewDidLoad
+{
+	[super viewDidLoad];
+}
+
+- (void)viewDidUnload
+{
+	[super viewDidUnload];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (IBAction)buttonTapped:(id)sender
+{
+	NSLog(@"You tapped on: %@", sender);
+
+	[self performSegueWithIdentifier:@"ModalSegue" sender:sender];
+}
+
+@end
